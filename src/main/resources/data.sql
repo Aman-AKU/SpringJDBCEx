8a314d5a6c222173814a854c7547b9410bd59c23
Aman-AKU <amankumarupadhyay89@gmail.com>
2024-12-01 22:49:36 +0530

    Add files via upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into student (rollNo,name,marks) values(101,'Kiran',79);
+
+insert into student (rollNo,name,marks) values(102,'Harsh',69);
+insert into student (rollNo,name,marks) values(103,'Sushil',82);
